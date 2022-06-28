b5fe8b95448c423b8562bf33d8025e938486b633
DavidMa1021 <david.malaver01@uptc.edu.co>
2022-06-27 20:00:58 -0500

    Agregado manejo de codigos de respuesta Inscripción de materias

--- Backend/build/controllers/inscriptionController.js
+++ Backend/build/controllers/inscriptionController.js
@@ -8,15 +8,48 @@ const database_1 = __importDefault(require("../database"));
 class InscriptionController {
     list(req, res) {
         const materias = database_1.default.query('select * from inscripcion_materias', (err, results, fields) => {
-            res.json(results);
             console.log(err);
-
+            if (err) {
+                res.status(400).json({
+                    status: 'error 400',
+                    message: err.message
+                });
+                return;
+            } else if(results.length == 0){
+                res.status(204).json({
+                    status: '204',
+                    result: 'No se encontró ningun registro que coincida con los parametros dados'
+                });
+            }else {
+                res.status(200).json({
+                    status: '200',
+                    result: results
+                });
+            }
         });
     }
     getId(req, res) {
+        console.log(req.params);
         const materias = database_1.default.query('select * from inscripcion_materias where id_materia = ? and id_estudiante = ?', [req.params.idm, req.params.ide], (err, results, fields) => {
-            res.json(results);
+            // res.json(results);
             console.log(err);
+            if (err) {
+                res.status(400).json({
+                    status: 'error 400',
+                    message: err.message
+                });
+                return;
+            } else if(results.length == 0){
+                res.status(204).json({
+                    status: '204',
+                    result: 'No se encontró ningun registro que coincida con los parametros dados'
+                });
+            }else {
+                res.status(200).json({
+                    status: '200',
+                    result: results
+                });
+            }
         });
     }
     create(req, res) {
@@ -41,19 +74,43 @@ class InscriptionController {
         res.json({ text: 'Eliminado una materia ' + req.params.idm });
     }
     update(req, res) {
-        const { id } = req.params;
-        database_1.default.query('UPDATE inscripcion_materias set ? WHERE id_materia = ? AND id_estudiante = ?', [req.body, req.params.idm, req.params.ide], (err, results, fields) => {
+        console.log(req.params);
+        database_1.default.query("UPDATE inscripcion_materias set ? WHERE id_materia = ? AND id_estudiante = ?",[req.body, req.params.idm, req.params.ide], (err, results, fields) => {
             console.log(err);
+            if (err) {
+                res.status(400).json({
+                    status: 'error 400',
+                    message: err.message
+                });
+                return;
+            } else {
+                res.status(200).json({
+                    status: '200',
+                    message: 'Se actualizó la inscripción de materia ' + req.params.idm
+                });
+            }
         });
-        res.json({ text: 'actualizando inscripción de materia ' + req.params.idm });
+        // res.json({ text: 'actualizando inscripción de materia ' + req.params.idm });
     }
     patch(req, res) {
         const { id } = req.params;
         database_1.default.query('UPDATE inscripcion_materias set estado_inscripción = ? WHERE id_materia = ? AND id_estudiante = ?', [req.body.estado_inscripción, req.params.idm, req.params.ide], (err, results, fields) => {
             console.log(err);
+            if (err) {
+                res.status(400).json({
+                    status: 'error 400',
+                    message: err.message
+                });
+                return;
+            } else {
+                res.status(200).json({
+                    status: '200',
+                    message: 'Se cambío el estado de la inscripción'
+                });
+            }
 
         });
-        res.json({ text: 'actualizando cambiado el estado de inscripcion de la materia ' + req.params.idm });
+        // res.json({ text: 'actualizando cambiado el estado de inscripcion de la materia ' + req.params.idm });
     }
 }
 exports.inscriptionController = new InscriptionController();
